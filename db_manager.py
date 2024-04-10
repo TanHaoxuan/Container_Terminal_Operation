05f5c9eeb09016aac781e580878177f19d2f59f5
TanHaoxuan <tanhaoxuan0519@gmail.com>
2024-04-10 20:32:54 +0800

    update the latest data schema and dataset

--- db_manager.py
+++ db_manager.py
@@ -22,12 +22,19 @@ db = engine.connect()
 def drop_all_tables(db: sqlalchemy.engine.Connection) -> None:
     db.execute(text(
         '''
-        DROP TABLE IF EXISTS movement;
-        DROP TABLE IF EXISTS ship_schedule;
-        DROP TABLE IF EXISTS ship;
-        DROP TABLE IF EXISTS container;
-        DROP TABLE IF EXISTS berth;
-        DROP TABLE IF EXISTS yard_location;
+            DROP TRIGGER IF EXISTS prevent_schedule_time_conflict ON ship_schedule;
+            DROP TRIGGER IF EXISTS prevent_movement_time_conflict ON movement;
+            DROP TRIGGER IF EXISTS check_last_operation_not_load ON movement;
+            DROP TRIGGER IF EXISTS check_ship_at_berth_schedule ON movement;
+            DROP TRIGGER IF EXISTS check_ship_at_berth_operation ON movement;
+            DROP TRIGGER IF EXISTS check_container_at_correct_location_during_operation ON movement;
+
+            DROP TABLE IF EXISTS movement;
+            DROP TABLE IF EXISTS ship_schedule;
+            DROP TABLE IF EXISTS ship;
+            DROP TABLE IF EXISTS container;
+            DROP TABLE IF EXISTS berth;
+            DROP TABLE IF EXISTS yard_location;
         '''
     ))
 
@@ -51,17 +58,13 @@ def create_all_tables(db: sqlalchemy.engine.Connection) -> None:
     ))
     db.execute(text(
         '''CREATE TABLE IF NOT EXISTS container (
-            iso_id VARCHAR(100) PRIMARY KEY,
-            weight DECIMAL(10, 2),
-            content VARCHAR(255),
-            owner VARCHAR(255)
+            iso_id VARCHAR(11) PRIMARY KEY,
+            content VARCHAR(255) NOT NULL
         );'''
     ))
     db.execute(text(
         '''CREATE TABLE IF NOT EXISTS berth (
-            berth_id INT PRIMARY KEY,
-            length DECIMAL(10, 2),
-            depth DECIMAL(10, 2)
+            berth_id INT PRIMARY KEY
         );'''
     ))
     db.execute(text(
@@ -73,67 +76,277 @@ def create_all_tables(db: sqlalchemy.engine.Connection) -> None:
         );'''
     ))
     db.execute(text(
+        '''CREATE TABLE IF NOT EXISTS ship_schedule (
+            schedule_id SERIAL PRIMARY KEY,
+            ship_mmsi INT NOT NULL,
+            berth_id INT NOT NULL,
+            e_arrival TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            e_departure TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            a_arrival TIMESTAMP WITHOUT TIME ZONE,
+            a_departure TIMESTAMP WITHOUT TIME ZONE,
+            FOREIGN KEY (ship_mmsi) REFERENCES ship(mmsi),
+            FOREIGN KEY (berth_id) REFERENCES berth(berth_id),
+            CHECK (e_arrival < e_departure AND a_arrival < a_departure)
+        );'''
+    ))
+    db.execute(text(
         '''CREATE TABLE IF NOT EXISTS movement (
             movement_id SERIAL PRIMARY KEY,
-            e_start_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-            e_end_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-            a_start_datetime TIMESTAMP WITHOUT TIME ZONE,
-            a_end_datetime TIMESTAMP WITHOUT TIME ZONE,
-            type VARCHAR(10) NOT NULL CHECK (type IN ('Load', 'Unload', 'Transfer')),
-            container_iso_id VARCHAR(100) NOT NULL,
-            ship_mmsi INTEGER,
-            src_bay INTEGER,
-            src_row INTEGER,
-            src_tier INTEGER,
-            des_bay INTEGER,
-            des_row INTEGER,
-            des_tier INTEGER,
+            container_iso_id VARCHAR(11) NOT NULL,
+            type VARCHAR(10) NOT NULL CHECK (type IN ('Unload', 'Transfer', 'Load')),
+            e_start TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            e_end TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            a_start TIMESTAMP WITHOUT TIME ZONE,
+            a_end TIMESTAMP WITHOUT TIME ZONE,
+            ship_mmsi INT,
+            src_bay INT,
+            src_row INT,
+            src_tier INT,
+            des_bay INT,
+            des_row INT,
+            des_tier INT,
             FOREIGN KEY (container_iso_id) REFERENCES container(iso_id),
             FOREIGN KEY (ship_mmsi) REFERENCES ship(mmsi),
             FOREIGN KEY (src_bay, src_row, src_tier) REFERENCES yard_location(bay, row, tier),
             FOREIGN KEY (des_bay, des_row, des_tier) REFERENCES yard_location(bay, row, tier),
-            CHECK (e_start_datetime < e_end_datetime)
+            CHECK (e_start < e_end AND a_start < a_end)
         );'''
     ))
     db.execute(text(
-        '''CREATE TABLE IF NOT EXISTS ship_schedule (
-            schedule_id SERIAL PRIMARY KEY,
-            ship_mmsi INTEGER NOT NULL,
-            berth_id INTEGER NOT NULL,
-            e_start_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-            e_end_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-            a_start_datetime TIMESTAMP WITHOUT TIME ZONE,
-            a_end_datetime TIMESTAMP WITHOUT TIME ZONE,
-            FOREIGN KEY (ship_mmsi) REFERENCES ship(mmsi),
-            FOREIGN KEY (berth_id) REFERENCES berth(berth_id),
-            CHECK (e_start_datetime < e_end_datetime)
-        );'''
-    ))
-    db.execute(text(
-        '''CREATE OR REPLACE FUNCTION prevent_time_conflict_func()
-        RETURNS TRIGGER AS $$
-        DECLARE
-            conflict_count INT;
-        BEGIN
-            -- Check for time overlap conflicts
-            SELECT COUNT(*)
-            INTO conflict_count
-            FROM ship_schedule
-            WHERE ship_mmsi = NEW.ship_mmsi
-            AND ((NEW.e_start_datetime BETWEEN e_start_datetime AND e_end_datetime)
-            OR (NEW.e_end_datetime BETWEEN e_start_datetime AND e_end_datetime));
+        '''
+            -- PREVENTS SCHEDULE EVENT FROM BEING CREATED IF THERE IS AN OVERLAP IN TIMING
+            -- WITH ANOTHER SCHEDULE OF THE SAME SHIP
+            CREATE OR REPLACE FUNCTION prevent_schedule_time_conflict_func()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                conflict_count INT;
+            BEGIN
+                -- Check for time overlap conflicts
+                SELECT COUNT(*)
+                INTO conflict_count
+                FROM ship_schedule
+                WHERE ship_mmsi = NEW.ship_mmsi
+                AND ((NEW.e_arrival BETWEEN e_arrival AND e_departure)
+                OR (NEW.e_departure BETWEEN e_arrival AND e_departure));
 
-            IF conflict_count > 0 THEN
-                RAISE EXCEPTION 'Time schedule conflict for ship % for %', NEW.ship_mmsi, NEW.e_start_datetime;
-            END IF;
-            RETURN NEW;
-        END;
-        $$ LANGUAGE plpgsql;
+                IF conflict_count > 0 THEN
+                    RAISE EXCEPTION 'Time schedule conflict for ship % at %', NEW.ship_mmsi, NEW.e_start;
+                END IF;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
 
-        CREATE TRIGGER prevent_time_conflict
-        BEFORE INSERT ON ship_schedule
-        FOR EACH ROW
-        EXECUTE FUNCTION prevent_time_conflict_func();'''
+            CREATE TRIGGER prevent_schedule_time_conflict
+            BEFORE INSERT ON ship_schedule
+            FOR EACH ROW
+            EXECUTE FUNCTION prevent_schedule_time_conflict_func();
+
+            -- PREVENTS MOVEMENT EVENT FROM BEING CREATED IF THERE IS AN OVERLAP IN TIMING
+            -- WITH ANOTHER MOVEMENT OF THE SAME CONTAINER
+            CREATE OR REPLACE FUNCTION prevent_movement_time_conflict_func()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                conflict_count INT;
+            BEGIN
+                -- Check for time overlap conflicts
+                SELECT COUNT(*)
+                INTO conflict_count
+                FROM movement
+                WHERE container_iso_id = NEW.container_iso_id
+                AND ((NEW.e_start BETWEEN e_start AND e_end)
+                OR (NEW.e_end BETWEEN e_start AND e_end));
+
+                IF conflict_count > 0 THEN
+                    RAISE EXCEPTION 'Time schedule conflict for container % for %', NEW.container_iso_id, NEW.e_start;
+                END IF;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER prevent_movement_time_conflict
+            BEFORE INSERT ON movement
+            FOR EACH ROW
+            EXECUTE FUNCTION prevent_movement_time_conflict_func();
+
+            -- PREVENTS MOVEMENT EVENT FROM BEING CREATED IF LAST KNOWN 
+            -- OPERATION OF THE CONTAINER IS A LOAD OPERATION
+            CREATE OR REPLACE FUNCTION check_last_operation_not_load_func()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                last_movement_type VARCHAR(10);
+            BEGIN
+                -- Check the last movement operation type for the container
+                SELECT type 
+                INTO last_movement_type
+                FROM movement
+                WHERE container_iso_id = NEW.container_iso_id
+                AND (NEW.e_start >= e_end OR
+                    NEW.e_start >= a_end)
+                ORDER BY a_end DESC
+                LIMIT 1;
+                -- If the last operation was 'Load', raise an exception
+                IF last_movement_type = 'Load' THEN
+                    RAISE EXCEPTION 'Container % was already loaded. Cannot insert new operation.', NEW.container_iso_id;
+                END IF;
+
+                -- If the check passes, allow the insert to proceed
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER check_last_operation_not_load
+            BEFORE INSERT ON movement
+            FOR EACH ROW
+            EXECUTE FUNCTION check_last_operation_not_load_func();
+
+            -- PREVENTS MOVEMENT EVENT FROM BEING CREATED IF THE SHIP WHICH CONTAINER IS 
+            -- UNLOADED FROM/LOADED TO IS NOT AT BERTH
+            CREATE OR REPLACE FUNCTION check_ship_at_berth_during_schedule()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                schedule_count INT;
+            BEGIN
+                -- Check if the ship is scheduled to be at the berth during the movement operation
+                SELECT COUNT(*)
+                INTO schedule_count
+                FROM ship_schedule
+                WHERE ship_mmsi = NEW.ship_mmsi
+                AND (NEW.e_start >= e_arrival
+                OR NEW.e_start >= a_arrival)
+                AND NEW.e_end <= e_departure;
+
+                -- If no schedule matches, raise an exception
+                IF schedule_count = 0 THEN
+                    RAISE EXCEPTION 'Ship % not scheduled at the berth during the expected time of operation (% to %). Unable to add operation for %.', 
+                    NEW.ship_mmsi, NEW.e_start, NEW.e_end, NEW.container_iso_id;
+                END IF;
+
+                -- If the check passes, allow the insert to proceed
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER check_ship_at_berth_schedule
+            BEFORE INSERT OR UPDATE ON movement
+            FOR EACH ROW
+            WHEN (NEW.type = 'Load' OR NEW.type = 'Unload')
+            EXECUTE FUNCTION check_ship_at_berth_during_schedule();
+
+            -- PREVENTS MOVEMENT EVENT FROM BEING UPDATED IF THE SHIP WHICH CONTAINER IS 
+            -- UNLOADED FROM/LOADED TO IS NOT AT BERTH
+            CREATE OR REPLACE FUNCTION check_ship_at_berth_during_operation()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                at_berth_count INT;
+            BEGIN
+                -- Check if the ship is at the berth (actual start time set, but no actual end time)
+                SELECT COUNT(*)
+                INTO at_berth_count
+                FROM ship_schedule
+                WHERE ship_mmsi = NEW.ship_mmsi
+                AND NEW.a_end IS NOT NULL
+                AND NEW.a_start >= a_arrival
+                AND (NEW.a_end IS NULL OR NEW.a_end <= a_departure)
+                AND a_end IS NULL;
+
+                -- If no such schedule exists, raise an exception
+                IF at_berth_count = 0 THEN
+                    RAISE EXCEPTION 'Ship % not scheduled at the berth during the expected time of operation (% to %). Unable to add operation for %.', 
+                    NEW.ship_mmsi, NEW.e_start, NEW.e_end, NEW.container_iso_id;
+                END IF;
+
+                -- If the check passes, allow the insert to proceed
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER check_ship_at_berth_operation
+            BEFORE UPDATE ON movement
+            FOR EACH ROW
+            WHEN (NEW.type = 'Load' OR NEW.type = 'Unload')
+            EXECUTE FUNCTION check_ship_at_berth_during_operation();
+
+            -- PREVENTS UNLOAD EVENT FROM BEING CREATED IF THE YARD LOCATION IS TAKEN AT TIME OF OPERATION
+            CREATE OR REPLACE FUNCTION check_yard_location_avaiable_func()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                location_is_available INT;
+            BEGIN
+                SELECT COUNT(*)
+                INTO location_is_available
+                FROM (
+                    -- Check for available yard locations
+                    SELECT bay, row, tier
+                    FROM yard_location
+                    EXCEPT
+                    SELECT l1.bay, l1.row, l1.tier
+                    FROM movement m1, (
+                        -- Latest operation timing concerning each yard location (if any)
+                        SELECT y.bay, y.row, y.tier, GREATEST(MAX(m.e_end), MAX(m.a_end)) AS latest
+                        FROM movement m, yard_location y
+                        WHERE ((y.bay=m.src_bay AND y.row=m.src_row AND y.tier=m.src_tier)
+                        OR (y.bay=m.des_bay AND y.row=m.des_row AND y.tier=m.des_tier))
+                        AND (m.a_end <= '2024-04-09 08:15:00' OR m.e_end <= '2024-04-09 08:15:00')
+                        GROUP BY (y.bay, y.row, y.tier)
+                    ) l1
+                    WHERE (m1.a_end = l1.latest OR m1.e_end = l1.latest)
+                    AND m1.des_bay=l1.bay AND m1.des_row=l1.row AND m1.des_tier=l1.tier
+                    AND (m1.type = 'Unload' OR m1.type = 'Transfer')
+                ) t
+                WHERE t.bay=NEW.des_bay AND t.row=NEW.des_row AND t.row=NEW.des_row;
+                
+                -- If there are, raise an exception
+                IF location_is_available = 0 THEN
+                    RAISE EXCEPTION 'Yard location is not available at start time of operation (%). Unable to insert movement for container %.', 
+                    NEW.e_start, NEW.container_iso_id;
+                END IF;
+
+                -- If the check passes, allow the insert to proceed
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER check_yard_location_avaiable
+            BEFORE INSERT OR UPDATE ON movement
+            FOR EACH ROW
+            WHEN (NEW.type = 'Unload')
+            EXECUTE FUNCTION check_yard_location_avaiable_func();
+
+            -- PREVENTS TRANSFER/LOAD EVENT FROM BEING CREATED IF THE CONTAINER IS NOT AT THE SPECIFIED YARD LOCATION
+            CREATE OR REPLACE FUNCTION check_container_at_correct_location_during_operation_func()
+            RETURNS TRIGGER AS $$
+            DECLARE
+                container_at_location VARCHAR(11);
+            BEGIN
+                -- Check if the container is at the yard location during operation time
+                SELECT container_iso_id
+                INTO container_at_location
+                FROM movement
+                WHERE (NEW.e_start >= e_end 
+                    OR NEW.e_start >= a_end)
+                AND des_bay = NEW.src_bay
+                AND des_row = NEW.src_row
+                AND des_tier = NEW.src_tier
+                ORDER BY e_end DESC
+                LIMIT 1;
+                
+                -- If containers do not match, raise an exception
+                IF container_at_location <> NEW.container_iso_id THEN
+                    RAISE EXCEPTION 'Container % is not at the expected yard location at start time of operation (%).', 
+                    NEW.container_iso_id, NEW.e_start;
+                END IF;
+
+                -- If the check passes, allow the insert to proceed
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER check_container_at_correct_location_during_operation
+            BEFORE INSERT OR UPDATE ON movement
+            FOR EACH ROW
+            WHEN (NEW.type = 'Transfer' OR NEW.type = 'Load')
+            EXECUTE FUNCTION check_container_at_correct_location_during_operation_func()
+        ;'''
     ))
 
     db.execute(text(
@@ -149,7 +362,7 @@ def load_dummy_data(db: sqlalchemy.engine.Connection) -> None:
     db.execute(text("".join(open("sql_data/berths.sql", "r").readlines())))
     db.execute(text("".join(open("sql_data/yard_locations.sql", "r").readlines())))
     #db.execute(text("".join(open("sql_data/ship_schedule.sql", "r").readlines())))
-    db.execute(text("".join(open("sql_data/movements.sql", "r").readlines())))
+    #db.execute(text("".join(open("sql_data/movements.sql", "r").readlines())))
     db.commit()
     print("ALL DATA POPULATED")
 
